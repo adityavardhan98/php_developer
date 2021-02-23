8394634d25b273622102dd5ef918ddc549a29b1b
adityavardhan98 <78993545+adityavardhan98@users.noreply.github.com>
2021-02-23 00:51:39 -0600

    Initial commit

---
+++ synder-recording_html/index.php
@@ -0,0 +1,360 @@
+<!DOCTYPE html>
+<html>
+ 
+<head>
+	<meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Synder Recording" name="keywords">
+    <meta content="Synder Recording" name="description">
+    <title>Synder Recording</title>
+
+    <!-- CSS Libraries -->
+	<link rel="icon" href="assets/images/logo/PNG/SRrecordings.png" type="image/gif" sizes="30x30">
+	<link rel="stylesheet" type="text/css" href="./assets/slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="./assets/slick/slick-theme.css">
+	<link rel="stylesheet" type="text/css" href="./assets/css/style.css" type="text/css"> 
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">	
+	<!-- JS Libraries -->
+	<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+	<script src="./assets/slick/slick.js" type="text/javascript" charset="utf-8"></script>
+	<script>
+		$(document).ready(function(){
+		  $("label span").click(function(){
+			$(".nav").toggleClass("show-nav");
+		  });
+		});
+	</script>
+</head>
+<body>
+<section class="section" id="header">
+ <div class="container">
+  <div class="row">
+   <div class="col-2 logo">
+	<a href="index.php"><img class="img-responsive" src="assets/images/logo/SVG/SRrecordings.svg" /></a>
+	<div class="menu-icon">
+	 <label for="check">
+      <input type="checkbox" id="check"/> 
+      <span></span>
+      <span></span>
+      <span></span>
+     </label>
+	</div>
+	</div>
+    <div class="col-10 nav">
+	 <ul>
+		<li><a href="index.php"><img class="img-responsive" src="assets/images/icons/SVG/home.svg" /> Home </a></li>
+		<li><a href="#"><img class="img-responsive" src="assets/images/icons/SVG/records.svg" /> Records </a></li>
+		<li><a href="#"><img class="img-responsive" src="assets/images/icons/SVG/music.svg" /> Music </a></li>
+		<li><a href="#"><img class="img-responsive" src="assets/images/icons/SVG/artists.svg" /> Artists </a></li>
+		<li><a href="#"><img class="img-responsive" src="assets/images/icons/SVG/concerts.svg" /> Concerts </a></li>
+		<li><a href="#" class="btn signUp"> Sign Up </a></li>
+	</ul>
+   </div>
+  </div>
+ </div>
+</section>
+
+<section class="section" id="banner">
+ <div class="container">
+  <div class="row">
+    <div class="col-12">
+		<h1>A.M. Paradox</h1>	
+		<p>Christopher Brent Wood, better known by his stage name Brent Faiyaz, isi
+		an American singer and record producer. He release debut project, an
+		extended play entitled A.M. Paradox, in 2016.</p>
+		<a href="" class="watchVideo"><img class="img-responsive" src="assets/images/icons/SVG/playbutton.svg" /> Watch Video</a>
+   </div>
+  </div>
+ </div>
+</section>
+
+<section class="section" id="brent-faiyaz">
+ <div class="container">
+  <div class="row">
+   <div class="col-6 brent-faiyaz-banner"> </div>
+    <div class="col-6">
+		<h2>Brent Faiyaz, So Far Gone</h2>	
+		<p>Faiyaz began uploading his music onto SoundCloud in 2014 and moved from his hometown Columbia, Maryland to Charlotte, North Carolina before ultimately settling in Los Angeles, California to further his music career. On January 19,2015, he released his debut single "Allure".[4] On June 1, 2016 Faiyaz released "Invite Me", the lead single from his upcoming debut EP. On September 19, 2016, his 21st birthday. the EP entitled A.M. Paradox was released and received with. positive reviews from music critics. On January 26, 2017. Sonder released their debut EP Into.</p>
+			<a href="#" class="btn">Learn More </a>
+   </div>
+  </div>
+ </div>
+</section>
+
+<section class="section" id="slider-section">
+  <div class="release">
+	<h2>New Releasese</h2>	
+	<p>New albums every single month, check out the newest & best from Snyder Recording artist, now available on Apple Music & Spotify.</p>
+	</div>
+  <div class="center slider">
+   <div class="sliderSlides">
+	<div class="row slider-slides">
+     <div class="col-4"><img class="img-responsive" src="assets/images/slider.png" /></div>
+     <div class="col-8">
+		<h2>Trapsoul</h2>
+		<h3>Bryson Tiller</h3>		
+		<p>Faiyaz began uploading his music onto SoundCloud in 2014 and moved from his hometown Columbia, Maryland to Charlotte.</p>
+	 </div>
+    </div> 
+   </div>
+    <div class="sliderSlides">
+	<div class="row slider-slides">
+     <div class="col-4"><img class="img-responsive" src="assets/images/slider.png" /></div>
+     <div class="col-8">
+		<h2>Trapsoul</h2>
+		<h3>Bryson Tiller</h3>		
+		<p>Faiyaz began uploading his music onto SoundCloud in 2014 and moved from his hometown Columbia, Maryland to Charlotte.</p>
+	 </div>
+    </div> 
+   </div>
+   <div class="sliderSlides">
+	<div class="row slider-slides">
+     <div class="col-4"><img class="img-responsive" src="assets/images/slider.png" /></div>
+     <div class="col-8">
+		<h2>Trapsoul</h2>
+		<h3>Bryson Tiller</h3>		
+		<p>Faiyaz began uploading his music onto SoundCloud in 2014 and moved from his hometown Columbia, Maryland to Charlotte.</p>
+	 </div>
+    </div> 
+   </div>
+   <div class="sliderSlides">
+	<div class="row slider-slides">
+     <div class="col-4"><img class="img-responsive" src="assets/images/slider.png" /></div>
+     <div class="col-8">
+		<h2>Trapsoul</h2>
+		<h3>Bryson Tiller</h3>		
+		<p>Faiyaz began uploading his music onto SoundCloud in 2014 and moved from his hometown Columbia, Maryland to Charlotte.</p>
+	 </div>
+    </div> 
+   </div>
+   <div class="sliderSlides">
+	<div class="row slider-slides">
+     <div class="col-4"><img class="img-responsive" src="assets/images/slider.png" /></div>
+     <div class="col-8">
+		<h2>Trapsoul</h2>
+		<h3>Bryson Tiller</h3>		
+		<p>Faiyaz began uploading his music onto SoundCloud in 2014 and moved from his hometown Columbia, Maryland to Charlotte.</p>
+	 </div>
+    </div> 
+   </div>
+   <div class="sliderSlides">
+	<div class="row slider-slides">
+     <div class="col-4"><img class="img-responsive" src="assets/images/slider.png" /></div>
+     <div class="col-8">
+		<h2>Trapsoul</h2>
+		<h3>Bryson Tiller</h3>		
+		<p>Faiyaz began uploading his music onto SoundCloud in 2014 and moved from his hometown Columbia, Maryland to Charlotte.</p>
+	 </div>
+    </div> 
+   </div>
+    </div>
+  </section>
+  
+ <section class="section" id="map">
+   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3101.9626626162026!2d-94.70662408480844!3d38.970520550235854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87c0eb66d8711493%3A0x128f5f4aeaee0907!2s10261%20W%2087th%20St%2C%20Overland%20Park%2C%20KS%2066212%2C%20USA!5e0!3m2!1sen!2sin!4v1613820587298!5m2!1sen!2sin" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</section>
+
+<section class="section" id="contact">
+  <div class="row">
+    <div class="col-8 contact-form">
+	 <div class="container">
+		<h2>Get In Touch</h2>
+		<p>New albums every single month, check out the newest & best from Snyder Recording artist, now available on Apple Music & Spotify.</p>
+		<form method="POST" action="">
+		<div class="row">
+		  <div class="col-6"> <input type="text" id="name" name="name" placeholder="First Name" required></div>
+		  <div class="col-6"> <input type="text" id="lname" name="lname" placeholder="Last Name" required></div>
+		  <div class="col-6"> <input type="email" id="email" name="email" placeholder="Email Address" required></div>
+		 <div class="col-6">
+			<div class="dropdown">
+        <div class="select">
+          <span>Reason For Contacting</span>
+          <i class="fas fa-sort-down"></i>
+        </div>
+        <input type="hidden" name="reason" required>
+        <ul class="dropdown-menu">
+          <li id="Reason">Reason</li>
+          <li id="Reason 2">Reason 2</li>
+		  <li id="Reason 3">Reason 3</li>
+        </ul> 
+       </div>
+		 </div>
+		 <div class="col-12"><textarea id="message" name="message"  rows="10" cols="" placeholder="Write a brief message" required></textarea></div>
+		 <div class="col-12"><input class="btn" type="submit" name="submit" value="send Message"></div>
+		</div>
+		</form>
+    </div>
+   </div>
+   <div class="col-4 contact-banner"> </div>
+  </div>
+</section>
+
+<section class="section" id="footer">
+ <div class="container">
+  <div class="row f-top">
+	<ul>
+		<li>
+		<a href="index.php" class="logo"><img class="img-responsive" src="assets/images/logo/SVG/SRRecordings-white.svg" /></a>
+		<p>8521 W Fairfax Rd
+		Los Angeles, CA 
+		90210
+		909.505.4302</p>
+		</li>
+		<li><h4>EXPLORE</h4> 
+			<a href="#">About Us</a> 
+			<a href="#"> Our Story</a> 
+			<a href="#"> Team</a> 
+			<a href="#"> Careers</a> 
+			<a href="#"> FAQ</a>
+		</li>
+		<li><h4>MUSIC</h4>
+			<a href="index.html">Missing Out</a> 
+			<a href="#">Trapsoul</a> 
+			<a href="#">Sonder Sons</a> 
+			<a href="#">Paradise Today</a> 
+			<a href="#">Needed</a>
+		</li>
+		<li><h4>ARTISTS</h4>
+			<a href="#">Brent Faiyaz</a> 
+			<a href="#">Drake</a> 
+			<a href="#">3ryson Tiller</a> 
+			<a href="#">Young Ford</a> 
+			<a href="#">Rory Fresco</a>
+		</li>
+		<li>
+			<h4>RECORDS</h4>
+			<a href="index.html">what a Time</a> 
+			<a href="#">Coming Home</a> 
+			<a href="#">Always First</a> 
+			<a href="#">So Far Gone</a> 
+			<a href="#">Birg Bluez</a>
+		</li>
+		<li><h4>CONCERTS</h4>
+			<a href="#">Midland Live</a> 
+			<a href="#">Granada 2018</a> 
+			<a href="#">Tiler Unplugged</a> 
+			<a href="#">Love Live</a> 
+			<a href="#">xX Live Mix</a>
+		</li>
+	</ul>	
+  </div>
+ <hr>
+  <div class="row f-bottom">
+	<div class="col-6 t-c">
+		<ul>
+		<li><a href="index.html">Privacy Policy</a></li>
+		<li><a href="#">Terms & Conditions</a></li>
+	</ul>
+   </div>
+   <div class="col-6 f-social">
+	<ul>
+		<li><a href="https://www.youtube.com/" target="_blank"><img class="img-responsive" src="assets/images/icons/SVG/youtube.svg" /></a></li>
+		<li><a href="https://www.twitter.com/" target="_blank"><img class="img-responsive" src="assets/images/icons/SVG/twitter.svg" /></a></li>
+		<li><a href="https://www.instagram.com/" target="_blank"><img class="img-responsive" src="assets/images/icons/SVG/Instagram.svg" /></a></li>
+		<li><a href="https://www.snapchat.com/" target="_blank"><img class="img-responsive" src="assets/images/icons/SVG/Snapchat.svg" /></a></li>
+	</ul>	
+   </div>
+  </div>
+ </div>
+</section>
+
+<script type="text/javascript">
+// slider
+    $(document).on('ready', function() {
+      $(".center").slick({
+        dots: false,
+		arrows: true,
+        infinite: true,
+        centerMode: true,
+		draggable: true,
+		autoplay: false,
+		speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+		responsive: [
+                    {
+                        breakpoint: 800,
+                        settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+						}
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }]
+			  });
+			});
+			
+			/*Form Reason Dropdown Menu*/
+				$('.dropdown').click(function () {
+						$(this).attr('tabindex', 1).focus();
+						$(this).toggleClass('active');
+						$(this).find('.dropdown-menu').slideToggle(300);
+					});
+					$('.dropdown').focusout(function () {
+						$(this).removeClass('active');
+						$(this).find('.dropdown-menu').slideUp(300);
+					});
+					$('.dropdown .dropdown-menu li').click(function () {
+						$(this).parents('.dropdown').find('span').text($(this).text());
+						$(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
+					});
+				/*End Dropdown Menu*/
+
+				$('.dropdown-menu li').click(function () {
+				  var input = '<strong>' + $(this).parents('.dropdown').find('input').val() + '</strong>',
+					  msg = '<span class="msg">Hidden input value: ';
+				  $('.msg').html(msg + input + '</span>');
+				}); 
+</script>
+
+
+<?php 
+// Semd Mail--------------
+if($_REQUEST['submit']){	
+$to = "adityamusuluri123@gmail.com";
+$subject     = "Contact Us"; 
+$sub1="noreply@http://microwebtechnology.com/synder-recording/";
+
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+// Create email headers
+$headers .= 'From: Synder Recording '.$sub1."\r\n".
+    'Reply-To: '.$_REQUEST['email']."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+ 
+// Compose a simple HTML email message
+$message = '<html><body>';
+
+$message .= '<p>'."  <br><b>First Name:- </b>".$_REQUEST['name']."</p>";
+$message .= '<p>'."  <br><b>Last Name:- </b>".$_REQUEST['lname']."</p>";
+$message .= '<p>'." <br><b>Reason For Contacting:- </b>" .$_REQUEST['reason']."</p>";
+$message .= '<p>'."  <br><b>Email Address:- </b>".$_REQUEST['email']."</p>";
+$message .= '<p>'."  <br><b>Message:- </b>".$_REQUEST['message']."</p>";
+
+$message .= '</body></html>';
+ 
+// Sending email
+if(mail($to, $subject, $message, $headers)){
+    //echo 'Your mail has been sent successfully.';
+	echo '<script>alert("Your mail has been sent successfully.")</script>'; 
+} else{
+    //echo 'Unable to send email. Please try again.';
+	echo '<script>alert("Unable to send email. Please try again.")</script>'; 
+}	
+} else{
+	echo 'Unable to send email. Please try again.';
+	//echo '<script>alert("Unable to send email. Please try again.")</script>'; 
+}
+	
+?>
+
+
+  </body>
+</html>
